75efd86fc3dc44dd13d8a69cbabe50b4fe3c22d3
dabaq01 <timothydabak@gmail.com>
2025-07-24 17:18:55 +0100

    task5.py

--- task5.py
+++ task5.py
@@ -10,3 +10,26 @@
     6. Find out what position "Paul" is in your final friends list (remember: position counting starts from 1 for humans!).
     7. arrange your contacts in Descending Alphabetical Order using.
 """
+#cleaning up your phone's contact list 
+friends = ["Aisha", "Daniel", "Esther", "John", "Mary", "Paul", "Ruth"]
+#add "Kemi" between "John" and "Mary"
+friends.insert(4, "Kemi")
+print (friends)
+#remove "Daniel"
+friends.remove(friends[1])
+print (friends)
+#Update "Aisha" to "Aisha_M"
+friends[0] = ("Aisha_M")
+print (friends)
+#add "Zainab" at the end of the list
+friends.append("Zainab")
+print (friends)
+#Create a new list with only the first 3 friends from your updated list and display it
+newFriends = friends[0:3]
+print (newFriends)
+#what position is "Paul" in on your final friends list
+paulPosition = friends.index("Paul")
+print (f"Paul's position on the contacts list is number:", paulPosition)
+#arrange your contacts in Descending Alphabetical Order
+friends.sort(reverse=True)
+print (f"the contacts list in descending order is:", friends)
